89ff41f8db44c280de4c449372d86642cc0b6c82
tuomaskuusisaari <tuomas.kuusisaari@gmail.com>
2020-04-14 23:06:51 +0300

    header design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import './App.css';
 
+const linkList = [
+  {
+    href: "https://www.youtube.com/watch?v=jMstFOpd-zA",
+    name: "Ympäristö"
+  },
+  {
+    href: "https://www.youtube.com/watch?v=jMstFOpd-zA",
+    name: "Taloudellisuus"
+  },
+  {
+    href: "https://www.youtube.com/watch?v=jMstFOpd-zA",
+    name: "Omavaraisuus"
+  },
+  {
+    href: "https://www.youtube.com/watch?v=jMstFOpd-zA",
+    name: "Tulevaisuus"
+  }
+]
+
+function Header() {
+
+
+  return(
+    <header>
+      {linkList.map((link) =>
+      (<a className="nav-link" href={link.href}>{link.name}</a>))}
+    </header>
+  )
+}
+
 function App() {
   return (
-      <header>
-      </header>
+    <body>
+      <Header/>
+    </body>
   );
 }
 
